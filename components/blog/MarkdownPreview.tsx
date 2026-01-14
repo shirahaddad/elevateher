ae5f955473a1640c68ccac26606827a544ad164e
Shira Haddad <shira.haddad@gmail.com>
2026-01-14 16:57:01 -0500

    fixed loading issue of workshop details page

--- components/blog/MarkdownPreview.tsx
+++ components/blog/MarkdownPreview.tsx
@@ -1,13 +1,5 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-import '@uiw/react-md-editor/markdown-editor.css';
-import '@uiw/react-markdown-preview/markdown.css';
-
-const MDPreview = dynamic(
-  () => import('@uiw/react-markdown-preview'),
-  { ssr: false }
-);
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 interface MarkdownPreviewProps {
   content: string;
@@ -15,27 +7,26 @@ interface MarkdownPreviewProps {
 
 export default function MarkdownPreview({ content }: MarkdownPreviewProps) {
   return (
-    <div className="wmde-markdown-var" data-color-mode="light">
-      <style jsx global>{`
-        .wmde-markdown-var ul {
-          list-style-type: disc;
-          padding-left: 2em;
-        }
-        .wmde-markdown-var ul li {
-          display: list-item;
-        }
-        .wmde-markdown-var ol {
-          list-style-type: decimal;
-          padding-left: 2em;
-        }
-        .wmde-markdown-var ol li {
-          display: list-item;
-        }
-        .wmde-markdown-var p {
-          white-space: pre-wrap;
-        }
-      `}</style>
-      <MDPreview source={content} />
+    <div className="max-w-none text-gray-800">
+      <ReactMarkdown
+        components={{
+          p: ({ node, ...props }) => (
+            <p className="whitespace-pre-wrap leading-7 mb-4" {...props} />
+          ),
+          ul: ({ node, ...props }) => (
+            <ul className="list-disc pl-8 mb-4 space-y-1" {...props} />
+          ),
+          ol: ({ node, ...props }) => (
+            <ol className="list-decimal pl-8 mb-4 space-y-1" {...props} />
+          ),
+          li: ({ node, ...props }) => <li className="list-item" {...props} />,
+          a: ({ node, ...props }) => (
+            <a className="text-purple-700 hover:text-purple-900 underline" {...props} />
+          ),
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 } 
