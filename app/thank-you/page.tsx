f708b8c45eb869fcbdd7ab49d589a40d4f24d77f
Shira Haddad <shira.haddad@gmail.com>
2026-02-25 14:35:11 -0500

    another build fix

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { Suspense, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 function ThankYouContent() {
   const searchParams = useSearchParams();
